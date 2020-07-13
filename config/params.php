bcab828c2a27dd2f522f4202069feff9b87f3aaa
kristina <nikkiwest1488@gmail.com>
2020-07-13 16:54:51 +0700

    add new smtp and new email address

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'nikkiwest1488@gmail.com',
-    'senderName' => 'Example.com mailer',
+    'adminEmail' => 'nikkiwest1488@gmail.com',
+    'senderEmail' => 'info@kristina-strelchuk.online',
+    'senderName' => 'Администратор',
 ];

54b3ef3e2b7028c17e6b115b3201f39b3cdde47d
nikkiwest <nikkiwest1488@gmail.com>
2021-01-27 17:37:51 +0700

    change title

--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Сайт-портфолио Кристины Стрельчук';
 ?>
     <!-- header -->
     <div id="home" class="header">
